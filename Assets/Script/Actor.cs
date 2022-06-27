493c9d5684a6ea1eeb8e6f7e9d4ddf5fe97c7dbd
j15935720033 <99215381@ulive.pccu.edu.tw>
2022-06-27 16:24:14 +0800

    update

---
+++ Assets/Script/Actor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Actor : MonoBehaviour
+{
+    #region 資料:保存系統需要的資料
+
+    protected int Blood//血量
+    {
+        get; set;
+    }
+
+    //private int mana
+    #endregion
+
+
+
+
+
+    #region 功能:實作該系統的複雜方法
+
+    #endregion
+
+
+
+
+
+
+    #region 事件:程式入口
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+}
