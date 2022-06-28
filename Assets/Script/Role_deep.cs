8a4f61f087bef8afec471caa490c218e0932457e
j15935720033 <99215381@ulive.pccu.edu.tw>
2022-06-28 21:51:43 +0800

    update

--- Assets/Script/Role_deep.cs
+++ Assets/Script/Role_deep.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class deep : Role
+public class Role_deep : Role
 {
     #region  屬性
     [SerializeField]
     private float speedWalk;
 
-    private Animator ani;
-    private Rigidbody2D rig;
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    private Transform transform;
     #endregion
 
     #region 事件:程式入口
     //喚醒事件:開始事件前執行一次，，取得元件等等
     private void Awake()
     {
-        ani = GetComponent<Animator>();
-        rig = GetComponent<Rigidbody2D>();
+        
+        animator= GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+
+        transform = animator.GetComponent<Transform>();
     }
     // Start is called before the first frame update
     void Start()
@@ -28,7 +33,7 @@ public class deep : Role
     //更新事件:每秒執行約60次，60FPS Frame per second
     void Update()
     {
-
+        Walk();
     }
     //一秒固定50次
     private void FixedUpdate()
@@ -37,13 +42,18 @@ public class deep : Role
     }
 
 
-
+    
 
 
     protected override void Walk()
     {
+        speedWalk = Input.GetAxis("Horizontal")* speedWalk;
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        speedWalk *= Time.deltaTime;
+        //rigidbody2D.velocity = new Vector2(speedWalk, rigidbody2D.velocity.y);
+        //rigidbody2D.AddForce(new Vector2(speedWalk, rigidbody2D.velocity.y));
         //設定加速度，rig.velocity.y-->rig中原本y軸的加速度
-        rig.velocity=new Vector2(speedWalk,rig.velocity.y);
+        rigidbody2D.velocity=new Vector2(speedWalk, rigidbody2D.velocity.y);
     }
     #endregion
 
